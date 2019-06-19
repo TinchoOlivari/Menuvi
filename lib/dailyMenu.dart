ed3afc21dccca0992cf67dcb504c6e917ded74b8
Martin Olivari <martin.olivari01@gmail.com>
2019-06-19 17:56:43 -0300

    Bug Fix: Dias sin menu y menus no disponibles

--- lib/dailyMenu.dart
+++ lib/dailyMenu.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 import 'package:menuvi/widgets/loading.dart';
 
 String principal = '';
@@ -13,6 +13,8 @@ String sopa = '';
 
 String fechaHoy = new DateFormat('dd-MM').format(DateTime.now());
 String diaHoy = new DateFormat('EEEE').format(DateTime.now());
+Future<DataSnapshot> databaseReference =
+    FirebaseDatabase.instance.reference().child(fechaHoy).once();
 
 final Widget svgIconMagdalena = SvgPicture.asset(
   "lib/assets/Magdalena.svg",
@@ -36,178 +38,299 @@ class _DailyMenuState extends State<DailyMenu> {
 
   @override
   Widget build(BuildContext context) {
+    if (DateTime.now().weekday == 6 || DateTime.now().weekday == 2) {
+      return Container(
+        padding: EdgeInsets.fromLTRB(20, 0, 20, 30),
+        child: Align(
+          alignment: Alignment.center,
+          child: NoMenu(),
+        ),
+      );
+    } else {
+      return Container(
+        padding: EdgeInsets.fromLTRB(20, 0, 20, 30),
+        child: Align(
+          alignment: Alignment.center,
+          child: MenuCreator(),
+        ),
+      );
+    }
+  }
+}
+
+class MenuCreator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.fromLTRB(20, 0, 20, 30),
-      child: Align(
-        alignment: Alignment.center,
-        child: menuCreator(),
+      child: Material(
+        color: Colors.white,
+        elevation: 2.0,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child: Column(
+                      children: <Widget>[
+                        Text(translateWeekday(),
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w300)),
+                        Text(
+                          fechaHoy,
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 70.0, right: 70.0),
+                child: Divider(
+                  color: Colors.grey,
+                  height: 10,
+                ),
+              ),
+              FutureBuilder(
+                  future: databaseReference.then((DataSnapshot snapshot) {
+                principal = snapshot.value['principal'].toString();
+              }), builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (principal == 'null') {
+                    return Container(
+                      child: Text('Plato principal no disponible',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 45,
+                              fontWeight: FontWeight.w500,
+                              fontStyle: FontStyle.italic)),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                    );
+                  } else {
+                    return Container(
+                      child: Text(principal,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 45,
+                            fontWeight: FontWeight.w500,
+                          )),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                    );
+                  }
+                } else {
+                  return Loading();
+                }
+              }),
+              FutureBuilder(
+                  future: databaseReference.then((DataSnapshot snapshot) {
+                opcion = snapshot.value['opcion'].toString();
+              }), builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (opcion == 'null') {
+                    return Container(
+                      child: Text('Plato opcional no disponible',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.grey)),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                    );
+                  } else {
+                    return Container(
+                      child: Text(
+                        opcion,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey),
+                      ),
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    );
+                  }
+                } else {
+                  return Container();
+                }
+              }),
+              Container(
+                child: svgIconMagdalena,
+                padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+              ),
+              FutureBuilder(
+                future: databaseReference.then(
+                  (DataSnapshot snapshot) {
+                    p_1 = snapshot.value['p_1'].toString();
+                    p_2 = snapshot.value['p_2'].toString();
+                    p_3 = snapshot.value['p_3'].toString();
+                  },
+                ),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (p_1 == 'null') {
+                      return Container(
+                        child: Text('Postres \nno disponibles',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 25, fontStyle: FontStyle.italic)),
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                      );
+                    } else {
+                      return Column(
+                        children: <Widget>[
+                          Padding(padding: EdgeInsets.all(1)),
+                          Text(
+                            p_1,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                          Text(
+                            p_2,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(1)),
+                          Text(
+                            p_3,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(1)),
+                        ],
+                      );
+                    }
+                  } else {
+                    return Loading();
+                  }
+                },
+              ),
+              Container(
+                child: svgIconSopa,
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
+              ),
+              FutureBuilder(
+                  future: databaseReference.then((DataSnapshot snapshot) {
+                sopa = snapshot.value['sopa'].toString();
+              }), builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (opcion == 'null') {
+                    return Container(
+                      child: Text('Sopa no disponible',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 30, fontStyle: FontStyle.italic)),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                    );
+                  } else {
+                    return Container(
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        "Sopa de " + sopa.toLowerCase(),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    );
+                  }
+                } else {
+                  return Loading();
+                }
+              }),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-menuCreator() {
-  Future<DataSnapshot> databaseReference =
-      FirebaseDatabase.instance.reference().child(fechaHoy).once();
-  return Container(
-    child: Material(
-      color: Colors.white,
-      elevation: 2.0,
-      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-      child: Container(
-        padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                  child: Column(
-                    children: <Widget>[
-                      Text(translateWeekday(),
+class NoMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Material(
+        color: Colors.white,
+        elevation: 2.0,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child: Column(
+                      children: <Widget>[
+                        Text(translateWeekday(),
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.w300)),
+                        Text(
+                          fechaHoy,
                           style: TextStyle(
-                              fontSize: 40, fontWeight: FontWeight.w300)),
-                      Text(
-                        fechaHoy,
-                        style: TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.w300),
-                      ),
-                    ],
+                              fontSize: 25, fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 70.0, right: 70.0),
+                child: Divider(
+                  color: Colors.grey,
+                  height: 10,
+                ),
+              ),
+              Container(
+                height: 350,
+                alignment: Alignment.center,
+                child: Text(
+                  "No hay menu disponible",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 70.0, right: 70.0),
-              child: Divider(
-                color: Colors.grey,
-                height: 10,
-              ),
-            ),
-            FutureBuilder(
-                future: databaseReference.then((DataSnapshot snapshot) {
-              principal = snapshot.value['principal'].toString();
-            }), builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Container(
-                  child: Text(principal,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 45,
-                        fontWeight: FontWeight.w500,
-                      )),
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
-                );
-              } else {
-                return Loading();
-              }
-            }),
-            FutureBuilder(
-                future: databaseReference.then((DataSnapshot snapshot) {
-              opcion = snapshot.value['opcion'].toString();
-            }), builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Container(
-                  child: Text(opcion,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey
-                      )),
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                );
-              } else {
-                return Container();
-              }
-            }),
-            Container(
-              child: svgIconMagdalena,
-              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-            ),
-            FutureBuilder(future: databaseReference.then(
-              (DataSnapshot snapshot) {
-                p_1 = snapshot.value['p_1'].toString();
-                p_2 = snapshot.value['p_2'].toString();
-                p_3 = snapshot.value['p_3'].toString();
-              },
-            ), builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Column(
-                  children: <Widget>[
-                    Text(
-                      p_1,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.all(1)),
-                    Text(
-                      p_2,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.all(1)),
-                    Text(
-                      p_3,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.all(1)),
-                  ],
-                );
-              } else {
-                return Loading();
-              }
-            }),
-            Container(
-              child: svgIconSopa,
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
-            ),
-            FutureBuilder(
-                future: databaseReference.then((DataSnapshot snapshot) {
-              sopa = snapshot.value['sopa'].toString();
-            }), builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Container(
-                  padding: EdgeInsets.only(bottom: 20),
-                  child: Text(
-                    sopa,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                );
-              } else {
-                return Loading();
-              }
-            }),
-          ],
+              )
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
 
 translateWeekday() {
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Stack(
